42d55fad9ac5a57f9239c8aae81c7c1a725a9a13
Preet Yadav <preet.yadav266@gmail.com>
2024-02-08 21:07:56 +0530

    changed the if statement which was causing error

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,22 +1,24 @@
 const jwt = require("jsonwebtoken")
-const JwtKey = require("../../assignments/week-3/04-mongo-with-jwt-auth/JWT/jwt")
+const { JWT_SECRET } = require("./config")
 
 function authMiddleware (req,res,next) {
     const authHeader = req.headers.authorization
-    // if(!authHeader || authHeader.startsWith("Bearer ")){
-    //     return res.status(403).json({})
-    // }
+
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(403).json({message:"auth is not present or the authorization doesn't start with Bearer"})
+    }
 
     const arr = authHeader.split(" ")
     const token = arr[1]
 
     try{
-        const decoded = jwt.verify(token,JwtKey)
+        const decoded = jwt.verify(token,JWT_SECRET)
         req.userId = decoded.userId
         next();
     }
     catch(err) {
-        return res.status(403).json({})
+        console.error(err)
+        return res.status(403).json({message : "some error orccured you are not verifed from the middleware"})
     }
 
 }
