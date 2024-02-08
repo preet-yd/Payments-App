213db652407f652071a8f46e8dfd59e9e538f8d6
Preet Yadav <preet.yadav266@gmail.com>
2024-02-08 21:08:57 +0530

    added the account route for api/v1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 app.use(cors());
 
 const userRouter = require("./user")
+const accountRouter = require("./account")
 router.use("/user",userRouter)
+router.use("/account",accountRouter)
 
 module.exports = router;
 
