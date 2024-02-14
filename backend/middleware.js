cbf6e8c6d4fc0724df98d964a7e02c0143dc22a7
Preet Yadav <preet.yadav266@gmail.com>
2024-02-14 16:49:40 +0530

    jwt promisified

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken-promisified"); // Use the alternative library
 const { JWT_SECRET } = require("./config")
 
 function authMiddleware (req,res,next) {
